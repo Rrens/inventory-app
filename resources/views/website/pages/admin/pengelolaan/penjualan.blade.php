c71dba49587b3501b884b2b4c70689e3df38a8c8
Rrens <yusufrendi313@gmail.com>
2024-07-31 20:18:14 +0700

    wes aman

--- resources/views/website/pages/admin/pengelolaan/penjualan.blade.php
+++ resources/views/website/pages/admin/pengelolaan/penjualan.blade.php
@@ -396,7 +396,7 @@
                                     if (checkRop <= rop) alert(
                                         'Quantity Mendekati ROP')
                                     // // console.log(ropValue)
-                                    // doSave(valueStock, productID, quantity, orderDate)
+                                    doSave(valueStock, productID, quantity, orderDate)
                                 }
                             })
 
